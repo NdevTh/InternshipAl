e66049f2910cd16ee323d0c0872a6d977caa0d41
ct pcStage <cth.huynh@gmail.com>
2023-05-16 15:56:32 +0200

    modified charts

--- src/echart/echartsUserLci2.js
+++ src/echart/echartsUserLci2.js
@@ -1,23 +1,55 @@
-
-var chartDom = document.getElementById("usr2");
+var chartDom = document.getElementById('usr2');
 var myChart = echarts.init(chartDom);
 var option;
 
 option = {
+
+  tooltip: {
+    trigger: 'axis'
+  },
+  legend: {
+    data: ['Email', 'Union Ads', 'Video Ads', 'Direct', 'Search Engine']
+  },
+  grid: {
+    left: '3%',
+    right: '4%',
+    bottom: '3%',
+    containLabel: true
+  },
+  toolbox: {
+    feature: {
+      saveAsImage: {}
+    }
+  },
   xAxis: {
     type: 'category',
+    boundaryGap: false,
     data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
   },
   yAxis: {
     type: 'value'
   },
   series: [
+   
     {
-      data: [150, 230, 224, 218, 135, 147, 260],
-      type: 'line'
+      name: 'Video Ads',
+      type: 'line',
+      stack: 'Total',
+      data: [150, 232, 201, 154, 190, 330, 410]
+    },
+    {
+      name: 'Direct',
+      type: 'line',
+      stack: 'Total',
+      data: [320, 332, 301, 334, 390, 330, 320]
+    },
+    {
+      name: 'Search Engine',
+      type: 'line',
+      stack: 'Total',
+      data: [820, 932, 901, 934, 1290, 1330, 1320]
     }
   ]
 };
 
-console.log("echart> settting option")
 if(option) myChart.setOption(option);
