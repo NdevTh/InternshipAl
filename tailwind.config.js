7a7f9e920145868a81b5c9ed5dd14d6259fa0933
Kaiiros <cth.huynh@gmail.com>
2023-06-19 10:56:31 +0200

    probleme resolv : tailwindconfig -> dlete theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,25 @@
 /** @type {import('tailwindcss').Config} */
-const defaultTheme = require("tailwindcss/defaultTheme");
+// const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
   content: ["./**/*.{html,js}",
-            "./index.html",],  
-  theme: {
-    screens :{
-      xs :"540px",
-      ...defaultTheme.screens,
-    },
+            "./index.html",
+          ],  
+            
+  // theme: 
+  // {
+  //   // screens :{
+  //   //   // xs :"540px",
+  //   //   ...defaultTheme.screens,
+  //   // },
 
-    extend: {
-      spacing :{
-        'some_key' :{
-          '1,5' : 'value',
-        }   
-      },
-    },
-  },
+  //   extend: {
+  //     spacing :{
+  //       'some_key' :{
+  //         '1,5' : 'value',
+  //       }   
+  //     },
+  //   },
+  // },
   variants:{},
   plugins: [],
 }
