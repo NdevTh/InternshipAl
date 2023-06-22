0923657c1e573238368233882e1ed50b2d50e9fe
Kaiiros <cth.huynh@gmail.com>
2023-06-22 16:34:21 +0200

    add new file indexCopy for test

--- src/page/pages.js
+++ src/page/pages.js
@@ -1,5 +1,4 @@
-// menuButton.addEventListener('click', function () {
-//     let classList = document.getElementById('nav').classList
-//     classList.toggle('hidden')
-//     classList.toggle('block')
-//   })
+function menuButton(e){
+    let list = document.querySelector('ul');
+    e.name === 'menu' ? (e.name = "close", list.classList.add('top-[80px]'), list.classList.add('opacity-100')): (e.name = "menu", list.classList.remove('top-[80px]'),list.classList.remove('opacity-100'))
+}
