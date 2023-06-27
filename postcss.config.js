cbd8b7573dfcd424b9c201a4afadb40aaf023136
Kaiiros <cth.huynh@gmail.com>
2023-06-27 13:01:16 +0200

    modified different files

--- postcss.config.js
+++ postcss.config.js
@@ -1,8 +1,22 @@
+const purgecss = require('@fullhuman/postcss-purgecss')({
+  // spécifiez les chemins vers tous les fichiers de modèle de votre projet
+  content: [
+      '../src/**/*.html',
+      '../src/**/*.vue',
+      '../src/**/*.jsx',
+      // etc.
+  ],
+
+  // cela est spécifique à Tailwind CSS
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
+});
+
+
 module.exports = {
-  plugins: {
-    style: {},
-    'postcss-import': {},
-    tailwindcss: {},
-    autoprefixer: {},
-  },
+  plugins: [
+    require('postcss-import'),
+    require('tailwindcss'),
+    require('autoprefixer'),
+    ...(process.env.NODE_ENV === 'production' ? [purgecss] : [])
+  ],
 }
